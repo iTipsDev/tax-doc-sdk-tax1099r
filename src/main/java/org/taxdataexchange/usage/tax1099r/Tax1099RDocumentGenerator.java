2f355dc808ef67d08fa9f061aaeb46f652547d7e
Bruce V Wilcox <brucevwilcox@itips.info>
2023-12-02 15:24:52 -0700

    2023-12-02 15:24

--- src/main/java/org/taxdataexchange/usage/tax1099r/Tax1099RDocumentGenerator.java
+++ src/main/java/org/taxdataexchange/usage/tax1099r/Tax1099RDocumentGenerator.java
@@ -56,7 +56,7 @@ public class Tax1099RDocumentGenerator {
         Tax1099RPdfBuilder pdfBuilder = new Tax1099RPdfBuilder( );
         byte[] bytes = pdfBuilder.buildQr( taxDataList );
 
-        String filePath = "samples/Tax1099R.sample.png";
+        String filePath = "samples/Tax1099R.qr.png";
         FileUtils.bytesToFile( bytes, filePath );
         System.out.println( filePath );
 
@@ -84,6 +84,12 @@ public class Tax1099RDocumentGenerator {
         String filePath = "samples/Tax1099R.sample.pdf";
         FileUtils.bytesToFile( pdfBytes, filePath );
         System.out.println( filePath );
+
+        String filePathPng = "samples/Tax1099R.sample.png";
+        byte[] pngBytes = Pdf2PngConverter.convertBytes( pdfBytes );
+        FileUtils.bytesToFile( pngBytes, filePathPng );
+        System.out.println( filePathPng );
+
 
     }
 
